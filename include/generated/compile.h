ddb802235a1c1b66240c7c8a5b42aa0efaf04567
peacetank200 <peacetank200@hanmail.net>
2013-03-03 14:53:44 +0900

    initial commit (SHW-M180L Opensource)

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Tue May 15 15:16:41 KST 2012"
+#define LINUX_COMPILE_TIME "15:16:41"
+#define LINUX_COMPILE_BY "hb0615choi"
+#define LINUX_COMPILE_HOST "oboe"
+#define LINUX_COMPILE_DOMAIN "oboe"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
